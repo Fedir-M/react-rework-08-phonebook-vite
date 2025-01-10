93c1a1949707a62f70aa372db7475ffc39e0a813
NormalOne <149543061+Fedir-M@users.noreply.github.com>
2025-01-10 13:52:24 -0500

    1st

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-rework-06-phonebook-vite/',
+  base: '/react-rework-08-phonebook-vite/',
 })
 //
